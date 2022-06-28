551737854997296d7dc57422929e86fa6111623b
kermitcosta <kermitcst@gmail.com>
2022-06-28 12:25:58 -0300

    Adicionada busca dos símbolos no formulário de conversão

--- src/app/components/conversor-page/conversor-form/conversor-form.component.ts
+++ src/app/components/conversor-page/conversor-form/conversor-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SupportedSymbols } from 'src/app/models/supported-symbols';
+import { ExchangeService } from 'src/app/services/exchange.service';
+
 
 @Component({
   selector: 'app-conversor-form',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversorFormComponent implements OnInit {
 
-  constructor() { }
+  symbols: SupportedSymbols[] = []
+  moedaOrigem = ''
+  moedaDestino = ''
+  valor = ''
+
+  constructor(private service: ExchangeService) { }
 
   ngOnInit(): void {
+    this.service.listSymbols().subscribe(dado => {
+      this.symbols = Object.values(dado.symbols)
+    })
   }
 
+  mostrarValor() {
+    console.log(this.moedaOrigem)
+    console.log(this.moedaDestino)
+    console.log(this.valor)
+  }
 }
--- src/app/components/conversor-page/conversor.module.ts
+++ src/app/components/conversor-page/conversor.module.ts
@@ -1,6 +1,11 @@
+import { ButtonLargeModule } from './../button-large/button-large.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorPageComponent } from './conversor-page.component';
 import { ConversorRoutes } from './conversor.routing';
@@ -15,7 +20,12 @@ import { ConversorFormComponent } from './conversor-form/conversor-form.componen
   imports: [
     CommonModule,
     ConversorRoutes,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    ButtonLargeModule
   ]
 })
 export class ConversorModule { }
