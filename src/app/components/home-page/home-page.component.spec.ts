4cd85100706ae2c1b38d3801c21ecba7bddea617
kermitcosta <kermitcst@gmail.com>
2022-07-11 20:55:14 -0300

    Adicionados testes para o home-page component

--- src/app/components/home-page/home-page.component.spec.ts
+++ src/app/components/home-page/home-page.component.spec.ts
@@ -2,13 +2,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HomeModule } from './home.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomePageComponent } from './home-page.component';
-
-
+import { Router } from '@angular/router';
 
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,10 +18,35 @@ describe('HomePageComponent', () => {
 
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router)
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('(D) should navigate to "/conversor" when "Converter Moedas" button is clicked', () => {
+    fixture.detectChanges()
+    const navigateSpy = spyOn(router, 'navigateByUrl')
+    const buttonConvert: HTMLButtonElement = fixture.nativeElement.querySelector('.btn-primary')
+
+    buttonConvert.click()
+    fixture.detectChanges()
+
+    expect((navigateSpy)).toHaveBeenCalledWith(router.createUrlTree(['/conversor']), jasmine.anything())
+
+  });
+
+  it('(D) should navigate to "/listar-moedas" when "Listar Símbolos" button is clicked', () => {
+    fixture.detectChanges()
+    const navigateSpy = spyOn(router, 'navigateByUrl')
+    const buttonConvert: HTMLButtonElement = fixture.nativeElement.querySelector('.btn-outline-primary')
+
+    buttonConvert.click()
+    fixture.detectChanges()
+
+    expect((navigateSpy)).toHaveBeenCalledWith(router.createUrlTree(['/listar-moedas']), jasmine.anything())
+
   });
 });
